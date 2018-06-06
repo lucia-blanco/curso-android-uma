331e8ad1e9c6e1313322cfbe0bf8595d76015d7c
Lucía Blanco <lublasal@gmail.com>
2018-06-06 12:53:52 +0200

    Ejercicio 11

--- Modulo 1/Relación preliminar/prEjercicio11.java
+++ Modulo 1/Relación preliminar/prEjercicio11.java
@@ -8,5 +8,24 @@ import java.util.Scanner;
 public class prEjercicio11 {
   public static void main(String[] args) {
     Scanner teclado = new Scanner(System.in);
+    int num;
+    int sum = 0;
+    boolean esPrimo = true;
+
+    do {
+      System.out.print("Introduce un número: ");
+      num = teclado.nextInt();
+      for (int i = 2; i < num; i++) {
+        if ((num % i) == 0) {
+          esPrimo = false;
+        }
+      }
+      if (esPrimo) {
+        sum += num;
+      }
+      esPrimo = true;
+    } while (num != 0);
+
+    System.out.println("La suma de los números primos introducidos es " +  sum);
   }
 }
