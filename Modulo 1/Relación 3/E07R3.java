82e8f8a20b8bf9c9d519c42999df8da6b6cb9fbf
lucia-blanco <lublasal@gmail.com>
2018-06-07 19:43:48 +0200

    Refactor

---
+++ Modulo 1/Relación 3/E07R3.java
@@ -0,0 +1,23 @@
+/*
+ * Ejercicio 7 | Relacion 3 | Modulo 1
+ * @author Lucia Blanco
+ */
+
+import java.util.Scanner;
+
+public class E07R3 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Introduce un número: ");
+        int num = teclado.nextInt();
+
+        System.out.print("El número " + num + " es");
+
+        if (num > 0) {
+            System.out.println(" positivo.");
+        } else {
+            System.out.println(" negativo.");
+        }
+    }
+}
