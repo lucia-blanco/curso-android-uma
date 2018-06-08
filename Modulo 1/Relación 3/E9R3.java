a493a397d2ffb2fe6d6c2fe441f8c6577433ecb6
lucia-blanco <lublasal@gmail.com>
2018-06-08 20:18:16 +0200

    Rel 4

--- Modulo 1/Relación 3/E9R3.java
+++ Modulo 1/Relación 3/E9R3.java
@@ -5,7 +5,7 @@
 
 import java.util.Scanner;
 
-public class E9R3 {
+public class E09R3 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         int day;
